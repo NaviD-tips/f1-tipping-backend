d569193a7ccf68114f6b5e819bd9cba9f50e5e6b
NaviD-tips <clintpmorrison@gmail.com>
2025-11-05 12:38:18 +1030

    updated login 3

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+// Log all incoming requests
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
